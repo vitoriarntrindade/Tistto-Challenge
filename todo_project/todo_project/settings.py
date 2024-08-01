aa94113495ce4c0b48524009b781dcc85ff41f09
vitoria <vitoriarntrindade@gmail.com>
2024-08-01 15:58:05 -0400

    feat: Adiciona app 'tasks' com Model Task, serializers e migrações
    - Criado um novo app Django chamado 'tasks' para gerenciamento de tarefas.
    - Implementado o Model Task para representar as tarefas no banco de dados.
    - Adicionados serializers para o Model Task para serialização dos dados.
    - Incluído o app 'tasks' no INSTALLED_APPS.
    - Realizadas as migrações do banco de dados para criar as tabelas correspondentes.

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Adicionar DRF
+    'tasks',  # Adicionar aplicativo tasks
 ]
 
 MIDDLEWARE = [
