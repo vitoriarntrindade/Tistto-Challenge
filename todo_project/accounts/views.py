5f4e27b3c35b9214fcce3f8d5570488a6be3c6c9
vitoria <vitoriarntrindade@gmail.com>
2024-08-01 15:46:11 -0400

    feat: Adiciona app 'accounts' com views de Registro, Login e Logout e rotas
    - Criado um novo app Django chamado 'accounts' para gerenciamento de usuários.
    - Implementada a view de Registro para cadastro de usuários.
    - Adicionada a view de Login para autenticação de usuários.
    - Desenvolvida a view de Logout para tratar o deslogamento de usuários.
    - Configuradas as rotas para as views acima no app 'accounts'.

---
+++ todo_project/accounts/views.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def register(request):
+    # Registrar novo usuário
+
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password)
+    return Response({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def user_login(request):
+    # Login de usuário
+
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def user_logout(request):
+    # Logout de usuário
+
+    logout(request)
+    return Response({'message': 'Logout successful'}, status=status.HTTP_200_OK)
