f6fcc8acc7b9bd013320582993007cd0a4ff7a8a
vitoria <vitoriarntrindade@gmail.com>
2024-08-03 18:58:54 -0400

    Implementação de roteamento privado para rotas protegidas

--- todo_frontend/src/App.js
+++ todo_frontend/src/App.js
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/Auth/Login";
-import TaskList from "./components/TaskList/TaskList";
-import Home from "./components/Home/Home"
+import Home from "./components/Home/Home";
 import "./styles.css";
-import TaskDetails from "./components/TaskDetails/TaskDetails"
-import TaskEdit from "./components/TaskEdit/TaskEdit"
+import TaskDetails from "./components/TaskDetails/TaskDetails";
+import TaskEdit from "./components/TaskEdit/TaskEdit";
 import RegisterForm from "./components/Auth/Register";
-
+import PrivateRoute from "./components/Auth/Auth";
 
 function App() {
   return (
@@ -14,9 +13,12 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/edit/:id" element={<TaskEdit />} />
-        <Route path="/tasks/:id" element={<TaskDetails />} />
+        <Route path="/" element={<PrivateRoute element={Home} />} />
+        <Route path="/edit/:id" element={<PrivateRoute element={TaskEdit} />} />
+        <Route
+          path="/tasks/:id"
+          element={<PrivateRoute element={TaskDetails} />}
+        />
       </Routes>
     </Router>
   );
